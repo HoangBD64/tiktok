e95a44c756389a0c9012d500cb29e71f226e92e3
Nguyễn Đình Hoàng <100840031+HoangBD64@users.noreply.github.com>
2024-07-22 15:12:50 +0700

    khung layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        
+      </div>
+    </header>;
 
-export default Header
+  };
+
+export default Header;
